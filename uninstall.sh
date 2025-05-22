81558ec3efb9f8dabe7b12019dd2312d75a34014
Cipher <91955270+Cipheritall@users.noreply.github.com>
2025-05-22 16:24:12 +0200

    fix: streamline uninstall script by removing unnecessary commands and improving cleanup process

--- uninstall.sh
+++ uninstall.sh
@@ -1,7 +1,2 @@
-rm -rf venv
-unlink /usr/bin/cipherLi
-rm -f $(pwd)/cipherLi
-echo "Désinstallation terminée. Tous les fichiers ont été supprimés."
-echo "Vous pouvez supprimer le répertoire de l'application si vous le souhaitez."
-echo "Merci d'avoir utilisé cipherLi !"
-echo "Si vous avez des questions ou des commentaires, n'hésitez pas à nous contacter."
+unlink /bin/cipherLi
+sudo rm -rf /opt/cipherLi
